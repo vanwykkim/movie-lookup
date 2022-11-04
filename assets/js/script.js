dd9a07a62c23649e82065c8d51e31805ab7e97d1
roddazad <roddazad@gmail.com>
2022-11-03 21:36:34 -0700

    worked on the dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,5 +56,27 @@ dropDownEl.on("click", function(){
     //this.content?
     getMovieData();
     loadMovieData();
+
 });
 
+//copied from w3
+
+// /* When the user clicks on the button, 
+// toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+  
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
